ef23b7f908d94ee899d6fb3d227ae3ae269e8f45
Aditya Kumar Verma <adikv0109@gmail.com>
2023-01-29 15:04:04 +0530

    Defined fields to store YT API data

---
+++ src/main/java/com/assignment/youtubeapi/youtube/YouTubeData.java
@@ -0,0 +1,42 @@
+package com.assignment.youtubeapi.youtube;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class YouTubeData {
+    String title;
+    String description;
+    String thumbnailUrl;
+    LocalDateTime dateTime;
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        DateTimeFormatter dateTimeFormatter= DateTimeFormatter.ofPattern("yyyy-MM-ddTHH:mm:ss");
+        this.dateTime = LocalDateTime.parse(dateTime,dateTimeFormatter);
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+}
