7ac4660e3db1fd885c300298da672c3096450b8a
Aditya Kumar Verma <adikv0109@gmail.com>
2023-01-29 21:39:19 +0530

    Modified sql file

--- youtubeapi_db.sql
+++ youtubeapi_db.sql
@@ -4,6 +4,7 @@ create user youtubeapi with password 'password';
 create database youtubeapidb with template=template0 owner=youtubeapi;
 \connect youtubeapidb;
 alter default privileges grant all on tables to youtubeapi;
+alter default privileges grant all on sequences to youtubeapi;
 
 create table youtube(
 id serial primary key,
