6c8df906f1416e03f5d03eea21a05a9e916c51c6
Aditya Kumar Verma <adikv0109@gmail.com>
2023-01-29 14:36:17 +0530

    Handler handles all the incoming API requests. Added the required API requests.

---
+++ src/main/java/com/assignment/youtubeapi/youtube/Handler.java
@@ -0,0 +1,32 @@
+package com.assignment.youtubeapi.youtube;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/youtube")
+public class Handler {
+
+    Service service;
+    @GetMapping("/all")
+    public ResponseEntity<List<YouTubeData>> getAll(
+            @RequestParam(value = "pageNumber", defaultValue = "1", required = false) Integer pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = "10", required = false) Integer pageSize
+    ){
+        try{
+            List<YouTubeData> data=service.getAllData(pageNumber,pageSize);
+            return new ResponseEntity<>(data, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/search/{keywords}")
+    public ResponseEntity<List<YouTubeData>> getResult(@PathVariable("keywords") String keywords){
+        return null;
+    }
+
+}
